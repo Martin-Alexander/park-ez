b52b018b201b5d0b4ed858f2bd5edbfe54d14385
Michael Zhu <michaelzhu63@hotmail.com>
2019-07-13 12:30:53 -0400

    Created plugins folder in app/JS

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,32 +1,27 @@
 class PlacesController < ApplicationController
   def index
-    address = params[:address].blank? ? "5333 Ave Casgrain" : params[:address]
-    distance = params[:distance].blank? ? 0.2 : params[:distance].to_i * 0.05
-    duration = params[:duration].blank? ? 2 : params[:duration].to_i
+    address = params[:address]
+    walking_radius = params[:walking]
+    hours = params[:hours]
 
     user_coords = {
       latitude: params[:user_latitude].to_f,
       longitude: params[:user_longitude].to_f
     }
 
-    destination = Geocoder.search(address + ", montreal")[0]
+    first_result = Geocoder.search(address + ", montreal")[0]
 
-    if destination
-      address_coordinates = { latitude: destination.data["lat"].to_f, longitude: destination.data["lon"].to_f }
+    if first_result
+      address_coordinates = { latitude: first_result.data["lat"].to_f, longitude: first_result.data["lon"].to_f }
     else
       address_coordinates = { latitude: 45.4900421, longitude: -73.5815461 } # backup
     end
 
-    places = Place
-      .joins(:periodes)
-      .near(address + ", montreal", distance)
-      .where("periodes.heure_debut < ? AND periodes.heire_fin > ?", Time.now, Time.now + duration.hour)
-      .uniq
+    places = Place.limit(10) # this is where the magic should happen ;)
 
     place_coordinates = places.map { |place| place.serializable_hash(only: [:longitude, :latitude]) }
 
     render json: { places: place_coordinates, destination: address_coordinates }
   end
-
   
 end
